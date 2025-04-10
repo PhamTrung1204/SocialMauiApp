90151cdb79b0d3cc02a5415afe0297fd5bb1808e
PhamTrung1204 <147811633+PhamTrung1204@users.noreply.github.com>
2025-04-10 14:08:53 +0700

    Update Post.cs

--- SocialMauiApp.Api/Data/Entities/Post.cs
+++ SocialMauiApp.Api/Data/Entities/Post.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 
 namespace SocialMauiApp.Api.Data.Entities
@@ -9,6 +9,8 @@ namespace SocialMauiApp.Api.Data.Entities
         public Guid Id { get; set; }
         public Guid UserId { get; set; }
         public virtual User User { get; set; }
+        public int LikeCount { get; set; }
+        public int CommentCount { get; set; }
         public string? Content { get; set; }
         [Comment("Physical path of the image")]
         public string? PhotoPath { get; set; }
