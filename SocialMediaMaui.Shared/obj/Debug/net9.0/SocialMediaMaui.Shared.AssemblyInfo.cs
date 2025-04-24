e8c1bd5b984a644abeb926dcbf91d49396dfc512
PhamTrung1204 <trungpham.31221024740@st.ueh.edu.vn>
2025-04-24 11:25:14 +0700

    Merge Trung to main

--- SocialMediaMaui.Shared/obj/Debug/net9.0/SocialMediaMaui.Shared.AssemblyInfo.cs
+++ SocialMediaMaui.Shared/obj/Debug/net9.0/SocialMediaMaui.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SocialMediaMaui.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e44eeef402dd570ecbc3a10947486fc79d42d089")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e2fe3dc6ed468cb2900b82f5b65eb683a64cf71")]
 [assembly: System.Reflection.AssemblyProductAttribute("SocialMediaMaui.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SocialMediaMaui.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
